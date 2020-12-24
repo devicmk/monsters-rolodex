bf06ac36598f98aa209c9f4285d640acf9449004
Marko Devic <devicmk@Markos-MacBook-Pro.local>
2020-12-24 18:46:56 +0100

    Updates

---
+++ precache-manifest.fab38a22905012e22ef3f220bbdd42e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adf4496592e5a781a2a7e081871d21f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "34a20a42219f0100a19f",
+    "url": "/monsters-rolodex/static/css/main.02ae696a.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a20a42219f0100a19f",
+    "url": "/monsters-rolodex/static/js/main.7835ce80.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
